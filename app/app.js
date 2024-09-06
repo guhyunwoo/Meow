a3c8324879f9f0ab77c65b867a4da873d366f70e
guhyunwoo <realization0327@gmail.com>
2024-09-06 14:15:29 +0900

    ADD:: 프론트 백 로그인

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 // 라우팅
 const home = require("./src/routes/home");
@@ -10,6 +11,9 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+// URL을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", home); // 미들웨어 등록
 
--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -11,5 +11,22 @@ function login() {
     id: id.value,
     psword: psword.value,
   };
-  console.log(req);
+  fetch("/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        location.href = "/";
+      } else {
+        alert(res.message);
+      }
+    })
+    .catch((err) => {
+      console.error("로그인 중 에러 발생");
+    });
 }
